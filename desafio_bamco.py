d535b30a79432c1d863e4bec4d8ffc9cd46f5179
cristovam <cristovam1712@gmail.com>
2024-10-09 01:42:48 -0300

    atualização1

--- desafio_bamco.py
+++ desafio_bamco.py
@@ -17,32 +17,33 @@
 menu = """
 
 
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
+[1] Depositar
+[2] Sacar
+[3] Extrato
+[4] Sair
 
 => """
 
 saldo = 0
-limite = 500
+limite = 1500
 extrato = ""
 numero_saques = 0
-LIMITE_SAQUES = 3
+LIMITE_SAQUES = 10
 
 while True:
     opcao = input(menu)
 
-    if opcao == "d":
+    if opcao == "1":
         valor = int(input("valor de deposito: "))
 
         if valor > 0:
             saldo += valor
             extrato += f"Deposito:R$ {valor:.2f}\n"
+            print(extrato)
         else:
             print("operacção invalida")
 
-    elif opcao == "s":
+    elif opcao == "2":
         valor = float(input("indique o valor desejado para saque: "))
 
         excedeu_saldo = valor > saldo
@@ -57,19 +58,23 @@ while True:
             print("vc utrapassou o limite de saques ou seja tentou savcar mais de 3 vezes")
             
         elif valor > 0:
+            saldo_anterior = saldo
             saldo -= valor
             extrato += f"Saque: R$ {valor:.2f}\n"
+            print(extrato)
+            saldo_retirado = saldo_anterior - saldo
+            print(f'saldo atual {saldo_anterior} - {saldo_retirado} = {saldo}')
             numero_saques += 1
         
         else:
             print("operação falhou")
         
-    elif opcao == "e":
+    elif opcao == "3":
         print("\n================EXTRATO=============")
         print("Não foram realizados movimentações." if not extrato else extrato)
         print(f"\nSaldo: R$ {saldo:.2f}")
         print("=======================================")
             
         
-    elif opcao == "q":
+    elif opcao == "4":
         break
